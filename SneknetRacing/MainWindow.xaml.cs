89b9cbf83f5aa3d2ba430879e35b7ccc3b3a78ab
Ihazevich <ihazevich@gmail.com>
2020-07-07 17:27:03 -0400

    Listener socket, commit from laptop

--- SneknetRacing/MainWindow.xaml.cs
+++ SneknetRacing/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,8 +23,14 @@ namespace SneknetRacing
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+        private const int _listenPort = 20777;
+        
+        public ManualResetEvent allDone = new ManualResetEvent(false);
+        
         public MainWindow()
         {
             var viewModel = new PacketMotionData();
@@ -35,6 +43,55 @@ namespace SneknetRacing
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            StartListener();    
+        }
+
+        private void StartListener()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPEndPoint localEP = new IPEndPoint(IPAddress.Parse("127.0.0.0"), _listenPort);
+
+            InfoLabel.Content = $"Local address and port : {localEP.ToString()}";
+
+            Socket listener = new Socket(localEP.Address.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+            try
+            {
+                listener.Bind(localEP);
+                listener.Listen(10);
+
+                while(true)
+                {
+                    allDone.Reset();
+
+                    InfoLabel.Content = "Waiting for a connection...";
+
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                    allDone.WaitOne();
+                }
+            }
+            catch (SocketException e)
+            {
+                InfoLabel.Content = e;
+            }
+            finally
+            {
+                listener.Close();
+            }
+
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            // Get the socket that handles the client request
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // Signal the main thread to continue
+            allDone.Set();
+
+            // Create the state object
         }
     }
 }
