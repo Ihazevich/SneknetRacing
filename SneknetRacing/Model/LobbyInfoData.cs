3ffb130cd5118781dbd45c6df333c718d92a6a1d
Ihazevich <ihazevich@gmail.com>
2020-07-13 10:52:13 -0400

    Lobby Info Packet

---
+++ SneknetRacing/Model/LobbyInfoData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SneknetRacing.Model
+{
+    public class LobbyInfoData : INotifyPropertyChanged
+    {
+        #region Fields
+        private byte _aiControlled;            // Whether the vehicle is AI (1) or Human (0) controlled
+        private byte _teamId;                  // Team id - see appendix (255 if no team currently selected)
+        private byte _nationality;             // Nationality of the driver
+        char[] _name;                // Name of participant in UTF-8 format – null terminated
+                                      // Will be truncated with ... (U+2026) if too long
+        private byte _readyStatus;             // 0 = not ready, 1 = ready, 2 = spectating
+        #endregion
+
+        #region Properties
+        public byte AIControlled
+        {
+            get
+            {
+                return _aiControlled;
+            }
+            set
+            {
+                _aiControlled = value;
+                OnPropertyChanged("AIControlled");
+            }
+        }
+        public byte TeamID
+        {
+            get
+            {
+                return _teamId;
+            }
+            set
+            {
+                _teamId = value;
+                OnPropertyChanged("TeamID");
+            }
+        }
+        public byte Nationality
+        {
+            get
+            {
+                return _nationality;
+            }
+            set
+            {
+                _nationality = value;
+                OnPropertyChanged("Nationality");
+            }
+        }
+        public char[] Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public byte ReadyStatus
+        {
+            get
+            {
+                return _readyStatus;
+            }
+            set
+            {
+                _readyStatus = value;
+                OnPropertyChanged("ReadyStatus");
+            }
+        }
+        #endregion
+
+        public LobbyInfoData()
+        {
+            Name = new char[48];
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ SneknetRacing/Model/PacketLobbyInfoData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace SneknetRacing.Model
+{
+    public class PacketLobbyInfoData : INotifyPropertyChanged
+    {
+        #region Fields
+        private PacketHeader _header;
+        private byte _numPlayers;
+        private LobbyInfoData[] _lobbyPlayers;
+        #endregion
+        
+        #region Properties
+        public PacketHeader Header
+        {
+            get
+            {
+                return _header;
+            }
+            set 
+            {
+                _header = value;
+                OnPropertyChanged("Header");
+            }
+        }
+        public byte NumPlayers
+        {
+            get
+            {
+                return _numPlayers;
+            }
+            set
+            {
+                _numPlayers = value;
+                OnPropertyChanged("NumPlayers");
+            }
+        }
+        public LobbyInfoData[] LobbyPlayers
+        {
+            get
+            {
+                return _lobbyPlayers;
+            }
+            set
+            {
+                _lobbyPlayers = value;
+                OnPropertyChanged("LobbyPlayers");
+            }
+        }
+        #endregion
+
+        public PacketLobbyInfoData()
+        {
+            LobbyPlayers = new LobbyInfoData[22];
+        }
+
+        public void Desserialize(byte[] data)
+        {
+            using (MemoryStream m = new MemoryStream(data))
+            {
+                using (BinaryReader reader = new BinaryReader(m))
+                {
+                    Header.PacketFormat = reader.ReadUInt16();
+                    Header.GameMajorVersion = reader.ReadByte();
+                    Header.GameMinorVersion = reader.ReadByte();
+                    Header.PacketVersion = reader.ReadByte();
+                    Header.PacketID = reader.ReadByte();
+                    Header.SessionUID = reader.ReadUInt64();
+                    Header.SessionTime = reader.ReadSingle();
+                    Header.FrameIdentifier = reader.ReadUInt32();
+                    Header.PlayerCarIndex = reader.ReadByte();
+                    Header.SecondaryPlayerCarIndex = reader.ReadByte();
+
+                    NumPlayers = reader.ReadByte();
+
+                    for(int i = 0; i < 22; i++)
+                    {
+                        LobbyPlayers[i] = new LobbyInfoData()
+                        {
+                            AIControlled = reader.ReadByte(),
+                            TeamID = reader.ReadByte(),
+                            Nationality = reader.ReadByte(),
+                            Name = reader.ReadChars(48),
+                            ReadyStatus = reader.ReadByte()
+                        };
+                    }
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
